d3b18866a747dea9debd934f21c8e728b2e5752e
Pranav <95164530+thefakepranav@users.noreply.github.com>
2022-07-06 12:23:19 +0530

    updated script

--- myScript.js
+++ myScript.js
@@ -1,6 +1,6 @@
-document.getElementById("btn1").onclick = function() {fun1()};
-document.getElementById("btn2").onclick = function() {fun2()};
-document.getElementById("reset").onclick = function() {fun3()};
+fun1();
+fun2();
+fun3();
 
 var str1 = "Lorem Ipsum Dolor <span>Lorem Ipsum</span>";
 var str_btn1 = "You have clicked button 1"
@@ -16,3 +16,7 @@ function fun2() {
 function fun3() {
     document.getElementById("heading").innerHTML=str1;
 }
+
+document.getElementById("btn1").onclick = function() {fun1()};
+document.getElementById("btn2").onclick = function() {fun2()};
+document.getElementById("reset").onclick = function() {fun3()};
